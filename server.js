7b0d802ba3024b51d1936f6b3bb93d7f7c2b1361
eonsyn <154956467+eonsyn@users.noreply.github.com>
2024-12-25 19:30:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello from Express!"); // Basic route to test
 });
-
+app.get("/home", async (req, res) => {
+  res.send("you are at home page .");
+});
 app.listen(2300, () => {
   console.log("Server is listening on port 2300");
 });
